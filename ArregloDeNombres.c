c558c06c17c5cd2ccb97d4ceee3f9f18e5f54b76
Octavio Selis <octavioselis12@gmail.com>
2026-04-17 19:21:39 -0300

    Punto 2, parte 2 completada

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 void MostrarPersonas(char *personas[],int cant);
+void BuscarNombre(char *nombres[],int id);
+char * BuscarNombreClave(char *nombres[],int cant,char *clave);
 
 int main(){
+    srand(time(NULL));
 
     char buff[50];
     char *nombres[5];
@@ -20,6 +24,15 @@ int main(){
     }
     MostrarPersonas(nombres,5);
 
+    int buscado;
+    printf("Busque un nombre mediante su ID: \n");
+    scanf("%d",&buscado);
+    BuscarNombre(nombres, buscado);
+
+    char * nombre_con_clave= BuscarNombreClave(nombres,5,"jose");
+
+    printf("Nombre buscado con clave: %s\n", nombre_con_clave);
+
     for(int i=0;i<5;i++){
         free(nombres[i]);
     }
@@ -34,5 +47,21 @@ void MostrarPersonas(char *personas[],int cant){
     }
 }  
 
+void BuscarNombre(char *nombres[],int id){
+    if(id>=5){
+        printf("No se encontro el valor buscado\n");
+    }
+    else{   
+        printf("-------- ID BUSCADO: %d --------\n",id);
+        printf("%s\n",nombres[id]);
+    }
+}
 
-
+char * BuscarNombreClave(char *nombres[],int cant,char *clave){
+    for(int i=0;i<cant;i++){
+        if(strstr(nombres[i],clave) !=NULL){
+            return nombres[i];
+        }
+    }
+    return "-1";
+}
